86f02fe9fe2e203337a0e803e5fbb686bbf2aa42
Ronald Bradford <ronald.bradford@gmail.com>
2022-02-04 10:48:37 -0500

    Small improvements to filters to find additional help commands

--- bin/generate-extra-help-commands.sh
+++ bin/generate-extra-help-commands.sh
@@ -11,7 +11,7 @@ readonly TMP_DIR="${TMP_DIR:-/tmp}"
 readonly EXTRA_COMMANDS_FILE="${EXTRA_COMMANDS_FILE:-extra-commands.cnf}"
 
 usage() {
-  echo "USAGE: $0 <product> <command> (Was given the arguments '$@')"
+  echo "USAGE: $0 <product> [<command>] (Was given the arguments '$@')"
   exit 1
 }
 
@@ -21,15 +21,19 @@ create_extra_commands_file() {
   local PRODUCT="$1"
   local ARGS="$2"
 
-  TMP_FILE="${TMP_DIR}/${PRODUCT}.tmp.$$"
-  ${PRODUCT} ${ARGS} | awk '{print $1}' > ${TMP_FILE}  2>/dev/null || (echo "ERROR: '${PRODUCT} ${ARGS}' is an invalid command" && exit 1)
+  local TMP_FILE="${TMP_DIR}/${PRODUCT}.tmp.$$"
+
+  # We only collect the first column of output
+  ${PRODUCT} ${ARGS} | awk '{print $1}' > ${TMP_FILE}  2>/dev/null || (echo "WARN: '${PRODUCT} ${ARGS}' returned [$?], continuing as this may be valid")
   [ ! -s "${TMP_FILE}" ] && echo "ERROR: '${PRODUCT} ${ARGS}' produced no output" && exit 1
 
   # Varying project cleanup
   # - Remove Blank Lines
   # - Remove --version
+  # - Remove lines starting with Usage
+  # - Remove lines starting with Available commands
   # - Remove ==> headers (brew)
-  sed -e "/^$/d;/--version/d;/==>/d;" ${TMP_FILE} > ${EXTRA_COMMANDS_FILE}
+  sed -e "/^$/d;/--version/d;/^Usage/d;/^Available/d;/==>/d;" ${TMP_FILE} > ${EXTRA_COMMANDS_FILE}
   rm -f ${TMP_FILE}
 
   echo "Generated '${EXTRA_COMMANDS_FILE}' with '$(cat ${EXTRA_COMMANDS_FILE} | wc -l | tr -d ' ')' entries"
@@ -51,7 +55,7 @@ run_extra_commands() {
 
 
 main() {
-  [ $# -lt 2 ] && usage "$@"
+  [ $# -lt 1 ] && usage "$@"
 
   PRODUCT="$1"
   shift
